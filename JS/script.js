94c08452dd1aa7cb009160cd05fff42f401aa561
Dhruv <sheredhruv@gmail.com>
2024-07-19 18:24:25 +0530

    Removing the Image

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,28 @@
-function locoCall() {
+document.addEventListener("DOMContentLoaded", function() {
     gsap.registerPlugin(ScrollTrigger);
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
     const locoScroll = new LocomotiveScroll({
         el: document.querySelector(".main"),
         smooth: true
     });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+
     locoScroll.on("scroll", ScrollTrigger.update);
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+
     ScrollTrigger.scrollerProxy(".main", {
         scrollTop(value) {
             return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        },
         getBoundingClientRect() {
             return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
         },
-        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
         pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
     });
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
+});
 
-}
 // locoCall()
 const api_url="./Api.js"
 async function showProjects(){
@@ -151,13 +149,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Function to show popup with language details
     function showPopup(skillData) {
         const LanguageNamePopup = document.getElementById('LanguageNamePopup');
-        const LanguageLogoPopup = document.getElementById('LanguageLogoPopup');
+        // const LanguageLogoPopup = document.getElementById('LanguageLogoPopup');
         const LanguageFounderPopup = document.getElementById('LanguageFounderPopup');
         const LanguageLaunchDatePopup = document.getElementById('LanguageLaunchDatePopup');
         const LanguageDescriptionPopup = document.getElementById('LanguageDescriptionPopup');
     
         LanguageNamePopup.textContent = skillData.name;
-        LanguageLogoPopup.src = skillData.logo;
+        // LanguageLogoPopup.src = skillData.logo;
         LanguageFounderPopup.textContent = `Founder: ${skillData.founder}`;
         LanguageLaunchDatePopup.textContent = `Launch Date: ${skillData.launch_date}`;
         LanguageDescriptionPopup.textContent = skillData.description;
